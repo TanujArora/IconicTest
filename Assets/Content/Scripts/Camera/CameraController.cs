f468a9ad719b1cd30e6de63c96eb9a0f650fdcc9
Tanuj Arora <tanuj.arora@encora.com>
2022-01-09 16:49:11 +0530

    Fixed Camera Controller and Updated Game Controller

---
+++ Assets/Content/Scripts/Camera/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class CameraController
+{
+	private const float FOLLOW_DIST_BACK = -10f;
+	private const float FOLLOW_DIST_UP = 10f;
+	private Transform cameraTrans;
+	private CharacterController character;
+
+	public CameraController(Camera camera, CharacterController character)
+	{
+		this.cameraTrans = camera.transform;
+		this.character = character;
+	}
+
+	public void LateUpdate()
+	{
+        cameraTrans.position = character.transform.position + new Vector3(0, FOLLOW_DIST_UP, FOLLOW_DIST_BACK);
+		cameraTrans.LookAt(character.transform);
+	}
+}
